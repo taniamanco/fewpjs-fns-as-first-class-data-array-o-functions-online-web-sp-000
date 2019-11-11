19fa10b3ac7985969cf1ba46fd2965c3a80e88c3
Tania Manco <belalov.tania16@gmail.com>
2019-11-11 19:06:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function wakeDog(){
-  
+function wakeDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
 function LeashDog(){
-  
+  console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
 function walkToPark(){
+  console.log(`Walk ${dogName} the ${dogBreed}`);
 }
 
 function throwFrisbee(){
-  
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
 function walkHome(){
-  
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
 function unLeashDog(){
-  
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
